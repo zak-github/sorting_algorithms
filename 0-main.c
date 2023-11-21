50648447b6d6a74dddfd32e9e619bf347d5aa7b1
zak-github <elmyar.zakaria@gmail.com>
2023-11-21 09:24:05 -0800

    tasks 1 veriv

--- 0-main.c
+++ 0-main.c
@@ -1,6 +1,5 @@
-/* main.c */
-
 #include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
 
 /**
@@ -13,15 +12,10 @@ int main(void)
     int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
     size_t n = sizeof(array) / sizeof(array[0]);
 
-    printf("Original array:\n");
     print_array(array, n);
-
-    printf("\nSorting array using Bubble Sort:\n");
+    printf("\n");
     bubble_sort(array, n);
-
-    printf("\nSorted array:\n");
+    printf("\n");
     print_array(array, n);
-
     return (0);
 }
-
