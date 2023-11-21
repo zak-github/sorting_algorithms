a60995fbf76ea049cc49b7f5f5a485602737259d
zak-github <elmyar.zakaria@gmail.com>
2023-11-21 09:57:58 -0800

    tasks 2

--- sort.h
+++ sort.h
@@ -5,9 +5,20 @@
 
 #include <stddef.h>
 
-/* Function prototypes */
+/* Function prototypes task0 */
 void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 
+/* Definition of a doubly linked list node */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *prev;
+    struct listint_s *next;
+} listint_t;
+
+/* Function prototypes task 1*/
+void print_list(const listint_t *list);
+void insertion_sort_list(listint_t **list);
 #endif /* SORT_H */
 
